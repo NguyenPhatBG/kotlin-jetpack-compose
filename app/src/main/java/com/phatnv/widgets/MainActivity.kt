fa23ba631277cc7b17380f90954fb8dd0e4f3878
phatnv <phat.nv@themansions.vn>
2023-10-27 14:29:28 +0700

    Add splash screen

--- app/src/main/java/com/phatnv/widgets/MainActivity.kt
+++ app/src/main/java/com/phatnv/widgets/MainActivity.kt
@@ -3,23 +3,54 @@ package com.phatnv.widgets
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.phatnv.widgets.src.MainScreen
 import com.phatnv.widgets.ui.theme.AppTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: SplashScreenViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // https://semicolonspace.com/jetpack-compose-splash-screen/
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.loading.value
+            }
+        }
+
         setContent {
             AppTheme() {
                 Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     MainScreen()
                 }
+            }
+        }
+    }
+
+    class SplashScreenViewModel : ViewModel() {
+        private val _loading = MutableStateFlow(true)
+        val loading = _loading.asStateFlow()
+
+        init {
+            viewModelScope.launch {
+                // run background task here
+                delay(2000)
+                _loading.value = false
             }
         }
     }
